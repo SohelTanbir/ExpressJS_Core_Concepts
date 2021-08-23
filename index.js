cb39959264e88981b5bf9752ca4e8092cae0742a
sohelTanbir <you@example.com>
2021-08-23 23:40:40 +0600

    http method practice and route

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Express js Core concepts
+//dependency
+const express = require('express');
+
+const app = express();
+
+const port = 5000;
+
+// get http method
+app.get('/', (req, res)=>{
+    res.send("Hello express world!")
+})
+
+// post http method
+// app.post('/post', (req, res)=>{
+//     res.send('I am post route')
+// })
+
+// put method
+app.put('/users', (req, res)=>{
+    res.send('PUT method')
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+})
